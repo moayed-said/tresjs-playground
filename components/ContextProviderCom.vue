830ded70945f8f7a3168b91ac4308e331b6d44df
moayed-said <moayed80023@gmail.com>
2025-11-21 21:35:05 +0200

    chore: delete unused files  and add package lock files in gitignore

--- components/ContextProviderCom.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-    import { useTresContext } from '@tresjs/core'
-    const {camera, scene} = useTresContext()
-    camera.value.position.z = 5;
-    camera.value.position.x = 0;
-    camera.value.position.y = 0;
-
-</script>
-
